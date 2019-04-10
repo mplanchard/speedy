b61fe2aa47cdd42937f21c238e02e1269660b5a3
Matthew Planchard <msplanchard@gmail.com>
2019-04-09 20:36:55 -0500

    drop indices

--- migrations/2019-03-26-020837_one_migration_to_rule_them_all/down.sql
+++ migrations/2019-03-26-020837_one_migration_to_rule_them_all/down.sql
@@ -4,3 +4,7 @@
 DROP TABLE IF EXiSTS posts;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS posts_tags;
+
+DROP INDEX IF EXISTS idx_posts_tags
+DROP INDEX IF EXISTS idx_posts_tags_post_id
+DROP INDEX IF EXISTS idx_posts_tags_tag_id
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ extern crate clap;
 extern crate diesel;
 extern crate warp;
 
-
-use clap::{Arg, ArgMatches, App, SubCommand};
+use clap::{App, Arg, ArgMatches, SubCommand};
 // use diesel::prelude::*;
 // use diesel::sqlite::SqliteConnection;
 // use warp::Filter;
-
 
 fn cli<'a>() -> ArgMatches<'a> {
     App::new("speedy")
@@ -17,19 +15,17 @@ fn cli<'a>() -> ArgMatches<'a> {
         .get_matches()
 }
 
-
 fn run() {
     let index = warp::fs::dir("static");
     warp::serve(index).run(([127, 0, 0, 1], 5000));
 }
 
-
 fn main() {
     let opts = cli();
     match opts.subcommand_name() {
-        Some("run") => { run() }
-        Some("add-post") => { println!("add-post") }
-        Some(_) => { println!("??") }
-        None => { run() }
+        Some("run") => run(),
+        Some("add-post") => println!("add-post"),
+        Some(_) => println!("??"),
+        None => run(),
     }
 }
